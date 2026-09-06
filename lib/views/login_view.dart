f3e9848fd98aad09d2aea8bdfeb05c52e1291b92
Yosef Dessalegn <yosefdessalegn490@gmail.com>
2026-09-06 12:03:22 +0300

    step 5

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtool show log;
-
 import 'package:mynotes/constants/routs.dart';
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -28,72 +27,84 @@ class _LoginViewState extends State<LoginView> {
     _password.dispose();
     super.dispose();
   }
- @override
-  Widget build(BuildContext context) {
-return  Scaffold(
-  appBar: AppBar(
-          title: const Text('Login'),
-          backgroundColor: Colors.blue,
-          foregroundColor: Colors.white,
-  ),
-  body: Column(
-            children: [
-              TextField(
-                controller: _email,
-                autocorrect: false,
-                enableSuggestions: false,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your email here',
-                ),
-              ),
-          
-              TextField(
-                controller: _password,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your password here',
-                ),
-              ),
-          
-              TextButton(
-                onPressed: () async {
-                  try{
-                    final email = _email.text;
-                    final password = _password.text;
-                    await FirebaseAuth.instance.signInWithEmailAndPassword(
-                    email: email,
-                    password: password,
-                  );
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    notesRoute,
-                    (route)=>false,
-                    );                
-                  } on FirebaseAuthException catch(e){
-                   if(e.code == 'invalid-credential'){
-                    devtool.log('Invalid-credential');
-                   }else if(e.code == 'wrong-password'){
-                    devtool.log('Wrong-password');
-                   }
-                  }
-                 
-                },
-                child: const Text('Login'),
-              ),
-              TextButton( onPressed: (){
-                Navigator.of(context).pushNamedAndRemoveUntil(
-                  registerRoute,
-                  (route) => false,
-                   );
-              },
-                child: const Text('Not registered yet? register here!'),
-              ),
-            ],
-          ),
-);
-  }
-  
-}
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            autocorrect: false,
+            enableSuggestions: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Enter your email here',
+            ),
+          ),
+
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: 'Enter your password here',
+            ),
+          ),
+
+          TextButton(
+            onPressed: () async {
+              try {
+                final email = _email.text;
+                final password = _password.text;
+                await FirebaseAuth.instance
+                    .signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                Navigator.of(
+                  context,
+                ).pushNamedAndRemoveUntil(notesRoute, (route) => false);
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'invalid-credential') {
+                  await showErrorDialog(
+                    context,
+                    'user not found or wrong password, please try again.',
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
+                }
+              } catch(e){
+                await showErrorDialog(
+                  context, 
+                  e.toString(),
+                  );
+              }
+            },
+            child: const Text('Login'),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerRoute,
+                (route) => false,
+              );
+            },
+            child: const Text(
+              'Not registered yet? register here!',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
